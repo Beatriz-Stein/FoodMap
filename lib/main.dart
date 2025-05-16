e650d2910ed2474ef485d5cd7f9435c23a80dc7e
Beatriz-Stein <Btrzstein5@gmail.com>
2025-05-16 12:08:30 -0300

    Adição da primeira imagem na tela inicial

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,20 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(MainApp());
 }
 
 class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+  MainApp({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+@override
+Widget build(BuildContext context) {
+  return MaterialApp(
+    home: Scaffold(
+      body: Center(
+        child: Image.asset("assets/img/entrada.png"),
       ),
-    );
-  }
+    ),
+  );
+}
 }
